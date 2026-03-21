392a509b6b417fa5150027410e0f95d6256aabd5
Prof. Cristiano <prof.cristianodepaula@gmail.com>
2026-03-21 15:36:45 -0300

    Adicionar arquivos de projeto.

---
+++ WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext // Herda de IdentityDbContext para incluir as funcionalidades de autenticação e autorização
+
+    {
+        // Sobrescreve o construtor para aceitar as opções de configuração do DbContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    }
+}
